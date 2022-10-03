fe0cadf2e621d79b387263181f8bea29c50fe6f1
tomit4 <mosssap@gmail.com>
2022-10-02 17:19:29 -0700

    :construction: Finished part 9 of tutorial

--- fem-manage-landing-page/main.js
+++ fem-manage-landing-page/main.js
@@ -9,3 +9,16 @@ navToggle.addEventListener('click', () => {
     primaryNav.toggleAttribute("data-visible")
     primaryHeader.toggleAttribute('data-overlay')
 })
+
+const slider = new A11YSlider(document.querySelector('.slider'), {
+    adaptiveHeight: true,
+    dots: true,
+    centerMode: true,
+    arrows: false,
+    responsive: {
+        480: {
+            dots: false,
+        }
+    }
+})
+
